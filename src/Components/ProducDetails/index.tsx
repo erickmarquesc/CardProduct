02fcc7282f70a2f4b89555c208f3887a668c544e
Erick Marques <erick.marquesc@hotmail.com>
2023-03-08 22:32:36 -0300

    feat: :sparkles: created a new component

---
+++ src/Components/ProducDetails/index.tsx
@@ -0,0 +1,12 @@
+import { ProductDetailsContainer } from "./styles";
+
+export function ProductDetails() {
+  return (
+    <ProductDetailsContainer>
+      <h4>CÓDIGO: 42404</h4>
+      <h1>Sofá Margot II - Rosé</h1>
+      <p>R$ 4.000</p>
+      <button>ADICIONAR À CESTA</button>
+    </ProductDetailsContainer>
+  );
+};
---
+++ src/Components/ProducDetails/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ProductDetailsContainer = styled.div`
+  button{
+    font-size: 12px;
+    line-height: 16px;
+
+    padding: 8px 16px;
+    border-radius: 999px;
+    border: 0.5px solid var(--text-color);
+
+    background-color: transparent;
+
+    margin-top: 20px;
+  };
+`;
